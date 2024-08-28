a7110b9415a7d4f6d27b14227a2e0a277560bf4b
Pedro Alexis Gonzalez Espinoza <pgonzaleze@vortex.corp>
2024-08-28 10:34:28 -0600

    cambios en el archivo para leer el token

---
+++ netflix/assets/main.js
@@ -0,0 +1,16 @@
+  // document.getElementById('activate-btn').addEventListener('click', function() {
+  //   const token = 'AQUÍ_VA_EL_TOKEN_PROVIDO';
+  //   const url = `http://tkn-act.megacable.com.mx:8238/api/token/${token}`;
+
+  //   fetch(url)
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       if (data.status === 'SUCCESS') {
+  //         alert('Token válido. Activando servicio...');
+  //         // Aquí puedes redirigir o realizar otra acción
+  //       } else {
+  //         alert('Token no válido.');
+  //       }
+  //     })
+  //     .catch(error => console.error('Error:', error));
+  // });
